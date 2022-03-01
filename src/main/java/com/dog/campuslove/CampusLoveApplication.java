9b5c657ef0df3256b9b64672a791996d6006353c
86180 <1094554173@qq.com>
2022-03-01 12:59:09 +0800

    init

---
+++ src/main/java/com/dog/campuslove/CampusLoveApplication.java
@@ -0,0 +1,13 @@
+package com.dog.campuslove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusLoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dog/campuslove/CampusLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dog.campuslove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusLoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
