646e9374ce286b723323fc08d3da5701dfeeda8e
86180 <1094554173@qq.com>
2022-04-16 23:35:34 +0800

    添加了userFeignClient

--- flipped-common/flipped-common-feign/src/main/java/com/campus/love/common/feign/module/user/UserFeignClient.java
+++ flipped-common/flipped-common-feign/src/main/java/com/campus/love/common/feign/module/user/UserFeignClient.java
@@ -3,6 +3,7 @@ package com.campus.love.common.feign.module.user;
 import com.campus.love.common.core.api.MessageModel;
 import com.campus.love.common.feign.domain.FeignConstant;
 import com.campus.love.common.feign.module.user.dto.SubscribedUserDto;
+import com.campus.love.common.feign.module.user.dto.UserInfoDto;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,4 +18,7 @@ public interface UserFeignClient {
     MessageModel<List<SubscribedUserDto>> querySubscribedInfo(Integer userId);
 
 
+    @ApiOperation("获取某用户的名字，头像")
+    @GetMapping(FeignConstant.FEIGN_INSIDE_URL_PREFIX+"/userInfo")
+    MessageModel<UserInfoDto> queryUserSomeInfos(Integer userId);
 }
---
+++ flipped-common/flipped-common-feign/src/main/java/com/campus/love/common/feign/module/user/dto/UserInfoDto.java
@@ -0,0 +1,14 @@
+package com.campus.love.common.feign.module.user.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class UserInfoDto {
+
+    @ApiModelProperty("用户姓名")
+    private String userName;
+
+    @ApiModelProperty("用户头像")
+    private String userAvatar;
+}
