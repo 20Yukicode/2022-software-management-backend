e7cc7a8c3e92d4a8288d48deee525da3f6fe8dfe
86180 <1094554173@qq.com>
2022-04-21 19:49:08 +0800

    UserFeignClient更新

--- flipped-common/flipped-common-feign/src/main/java/com/campus/love/common/feign/module/user/UserFeignClient.java
+++ flipped-common/flipped-common-feign/src/main/java/com/campus/love/common/feign/module/user/UserFeignClient.java
@@ -6,6 +6,7 @@ import com.campus.love.common.feign.module.user.dto.SubscribedUserDto;
 import com.campus.love.common.feign.module.user.dto.UserInfoDto;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -14,7 +15,7 @@ import java.util.List;
 @FeignClient("flipped-user")
 public interface UserFeignClient {
 
-    @ApiOperation("获取关注着的信息")
+    @ApiOperation("获取关注者的信息")
     @GetMapping(FeignConstant.FEIGN_INSIDE_URL_PREFIX + "/subscribed")
     MessageModel<List<SubscribedUserDto>> querySubscribedInfo(Integer userId);
 
@@ -24,6 +25,11 @@ public interface UserFeignClient {
     MessageModel<UserInfoDto> queryUserInfos(Integer userId);
 
 
+    /**
+     * 这个还没想好
+     * @param userId
+     * @return
+     */
     @ApiOperation("根据用户标签来筛选动态")
     @GetMapping(FeignConstant.FEIGN_INSIDE_URL_PREFIX + "/{userId}")
     MessageModel<List<Integer>> queryAccordingToLabel(@PathVariable Integer userId);
