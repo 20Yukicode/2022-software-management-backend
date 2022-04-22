d90ac9b078dffdd22df29a4e8f58f10037d936d4
86180 <1094554173@qq.com>
2022-04-22 19:53:30 +0800

    enableFeignClient

--- flipped-tweet/src/main/java/com/campus/love/tweet/TweetServiceApplication.java
+++ flipped-tweet/src/main/java/com/campus/love/tweet/TweetServiceApplication.java
@@ -2,8 +2,10 @@ package com.campus.love.tweet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.campus.love")
+@EnableFeignClients(basePackages = "com.campus.love.common.feign")
 public class TweetServiceApplication {
 
     public static void main(String[] args) {
